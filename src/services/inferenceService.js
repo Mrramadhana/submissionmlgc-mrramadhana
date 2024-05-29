66fc82d6274bb18a426d920112d761c650ad8be6
yusrilprayoga-code <124210006@student.upnyk.ac.id>
2024-05-29 18:50:18 +0700

    Update code storeData

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -1,26 +1,40 @@
 const tf = require('@tensorflow/tfjs-node');
 const InputError = require('../exceptions/InputError');
- 
+
 async function predictClassification(model, image) {
-    try { 
+    try {
         const tensor = tf.node
             .decodeJpeg(image)
             .resizeNearestNeighbor([224, 224])
             .expandDims()
-            .toFloat()
-        
+            .toFloat();
+
         const prediction = model.predict(tensor);
-        const probabilities = await prediction.data();
-        const classes = ['Cancer', 'Non-cancer'];
-        
-        const predictedClassIndex = probabilities[0] > 0.5 ? 0 : 1;
-        const label = classes[predictedClassIndex];
-        
-        return { label };
+        const score = await prediction.data();
+        const confidenceScore = Math.max(...score) * 100;
+
+        const isCancer = confidenceScore > 50;
+        const classes = ['Non-cancer', 'Cancer'];
+
+        const label = classes[isCancer ? 1 : 0];
+
+        let suggestion;
+
+        if (isCancer) {
+            suggestion = 'Segera berkonsultasi dengan dokter spesialis kulit terdekat.';
+        } else {
+            suggestion = 'Tetap jaga kesehatan kulit Anda.';
+        }
+
+        return {
+            label,
+            confidenceScore,
+            suggestion
+        };
+
     } catch (error) {
-        throw new InputError(`Terjadi kesalahan input: ${error.message}`)
+        throw new InputError(`Terjadi kesalahan dalam melakukan prediksi`);
     }
-  
 }
- 
+
 module.exports = predictClassification;
